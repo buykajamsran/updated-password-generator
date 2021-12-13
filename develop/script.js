f9e2d8fb2376da38a76aa96b0135b2beffb191d9
buykajamsran <buyka.jamsran@gmail.com>
2021-12-12 21:24:54 -0600

    Variables for letters, number, and special characters added

--- develop/script.js
+++ develop/script.js
@@ -3,18 +3,28 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var wordlength;
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumber;
+var confirmSpecialcharacter;
 
 function generatePassword() {
     
 // 1. Prompt the user for the password criteria
 //  a. Password length 8 - 128
-enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-    if (!enter) {
-        alert("This needs a value");
-    } else if (enter < 8 || enter > 128) {
-        enter = parseInt(prompt("You must choose between 8 and 128"));
+wordlength = parseInt(prompt("Choose length of the password"));
+    if (!wordlength) {
+        alert("Add value");
+    } else if (wordlength < 8 || wordlength > 128) {
+        wordlength = parseInt(prompt("Choose a length of at least 8 characters and no more than 128 characters"));
     };
 //  b. Lowercase, uppsercase, numbers, special characters
+lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+specialcharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "~"];
+
 // 2. Validate the input
 // 3. Generate password based on criteria
 
